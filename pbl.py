2d9720f0baecfd4eb621e5a120c8e3bd66d59b34
leonardogamateixeira <leoteixeirastack@outlook.com>
2024-04-15 13:23:22 -0300

    adciona a declaração de não plagio

--- pbl.py
+++ pbl.py
@@ -1,3 +1,14 @@
+
+# Autor: Leonardo Gama Teixeira
+# Componente Curricular: 2024.1 EXA854 - MI - ALGORITMOS (TP03) 
+# Concluido em: 15/04/2024
+# Declaro que este código foi elaborado por mim de forma individual e não contém nenhum
+# trecho de código de outro colega ou de outro autor, tais como provindos de livros e
+# apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
+# de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
+# do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.
+
+
 # Essa parte é apenas para qual menu inicial, onde o usuario seleciona
 # se vai configurar o progama, usar a versão padrão(a do problema) 
 # ou encerrar o progama
