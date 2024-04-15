80a0b9e29c0f62c1f80afb21d293a4c83b97006c
leogamateixeira <leoteixeirastack@outlook.com>
2024-04-15 01:15:31 -0300

    adcionando todas as saidas, tirando coisas inuteis, reorganizando o codigo

--- pbl.py
+++ pbl.py
@@ -1,42 +1,39 @@
-# Essa parte é apenas para qual menu o usuario quer ir
+# Essa parte é apenas para qual menu inicial, onde o usuario seleciona
+# se vai configurar o progama, usar a versão padrão(a do problema) 
+# ou encerrar o progama
 NumOpMenu = int(input('''
--------Selecione uma opção---------
+-------Menu de inicial---------
+                      
 1- Para configurar o progama
 2- Para usar versão padrão(Evento EngComp da UEFS)
 3- Para encerrar o progama
-'''))
+                      
+Selecione uma opção: '''))
 
 match NumOpMenu:
-# Vai para a tela de configuração do progama
-
-    case 1:
-        RespOpMenu = 2
-        while RespOpMenu == 2:           
-# Case 1, o progama será configurado e depois irá para a tela de vendas
+# Após o usuario selecionar uma opção alguma das opções será iniciada
+    case 1:          
+# O progama será configurado pelo usuario e depois
+# o progama irá para a tela de vendas
             ingressos = int(input("Digite a quantidade de ingressos: "))
             precoInt = float(input("Digite o preço do ingresso: R$"))
             precoMeia = float(input("Digite o preço da meia-entrada: R$"))
             precoDesc = float(input("Caso aja algum desconto digite-o, caso não digite 0: R$"))
-
-            print(f"Você configurou a quantidade de ingressos como {ingressos}, o preço á R${precoInt},\n a meia-entrada á R${precoMeia} e o desconto como R${precoDesc}, está tudo correto?")
-            RespOpMenu = int(input("1- Sim \n2- Não\n")) 
-# Adcionei a var RespOpMenu caso o usuario tenha digitado algo errado e queira retornar para a tela de configuração. 
     case 2:
-# A versão do problema proposto vai ser selecionada e as os valores serão atribuidos as variaveis
+# Versão do problema proposto vai ser selecionada e as os valores
+# do problema serão utilizados nas respectivas variáveis
         print("Versão padrão selecionada\n")
         ingressos = 2000
         precoInt = 30      
         precoMeia = 15
         precoDesc = 10
     case 3:
+# Encerra o progama
         print("Encerrando progama")
         exit()
-    
-    case _:
-        print("Opção invalida\nEncerrando progama")
-        exit()
 
-# Após selecionar ou a versão ou configurar começa a tela de vendas onde eu pré-criei algumas variaveis fora do while para guardar as informações e elas não serem perdidas nos loops
+# Após selecionar ou a versão ou configurar começa a tela de vendas onde eu pré-criei algumas
+# variaveis fora do while para guardar as informações e elas não serem perdidas nos loops
 
 IntVendas = 0
 DAcortesias = 0
@@ -50,13 +47,13 @@ while ingressos > 0 and EventoFim > 1:
     NumOpIng = int(input('''
 -------Tela de Vendas------
 
-Selecione um tipo de ingresso:
 1- Inteira
 2- Meia-entrada(Estudante)
 3- Meia-entrada(idoso)
 4- Descontos
 5- Cortesia
-'''))
+                         
+Selecione um tipo de ingresso: '''))
 # o usuario seleciona qual tipo de ingresso será vendido e pergunta quantos ingressos foram vendidos e soma suas idades com o uma estrutura de repetição, que repetira a pergunta de idade para cada ingresso que foi vendido.
     VendasIng = 0
     VendasIng = int(input("Quantos ingressos foram vendidos? "))
@@ -139,15 +136,12 @@ Total de meia-entradas para estudantes vendidas: {EstudanteVendas};\n
 Total de meia-entradas para idosos vendidas: {IdosoVendas};\n
 Total de inteiras vendidas: {IntVendas};\n
 Total de descontos vendidos: {DescontoVendas};\n
-Cortesias para DA e convidados: {Cortesia};\n
+Cortesias para DA e convidados: {DAcortesias};\n
+Total de cortesias para vendedores comissionados: {Cortesia};
 Total de dinheiro arrecadado: R${RsTotal};\n
 Total de dinheiro das meia-entradas: R${RsMeia};\n
 Total de dinheiro das inteiras: R${RsInt};\n
 Total de dinheiro das descontos: R${RsDesc};\n
 Ingresso mais vendido: {MaisVendas};\n
 Média de idade dos compradores: {round(MediaIdade, 2)} 
-''')                         # ver esse round se ta certo msm
-
-# quantidade de cortesias para integrantes do DA e convidados
-# quantidade total de cortesias para vendedores comissionados
-# quantidade de ingressos vendidos por vendedores do curso de Biologia e de Enfermagem
+''')
